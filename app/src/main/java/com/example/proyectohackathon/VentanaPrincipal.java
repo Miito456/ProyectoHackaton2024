ce3dc970429e12c771c406ae8d8ae3dc8fa47949
Mito 456 <Mito5524@gmail.com>
2024-10-11 01:47:55 -0600

    Agregamos VentanaPrincipal

---
+++ app/src/main/java/com/example/proyectohackathon/VentanaPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.proyectohackathon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VentanaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_principal);
+    }
+}
